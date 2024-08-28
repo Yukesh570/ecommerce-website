d6decae41acdfd92cb596fb351780550c12af1bd
yukesh570 <yukesh.maha10@gmail.com>
2024-08-28 17:58:12 +0545

    smartgateway

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -8,8 +8,6 @@ class UserSerializer(serializers.ModelSerializer):
     name= serializers.SerializerMethodField(read_only=True)
     _id= serializers.SerializerMethodField(read_only=True)
     isAdmin= serializers.SerializerMethodField(read_only=True)
-
-
     class Meta:
         model=User
         fields=['_id', 'username','email','name','isAdmin']
